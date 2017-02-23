4a46ccbf15bd11af7bb222421044e809bc775ab0
Sunny <sunny@Sunnys-MacBook-Air.local>
2017-02-24 01:24:53 +0530

    requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -y install redis-server
+sudo apt-get install pip
+pip install bs4
+pip install redis
+pip install pandas
+pip install requests
+pip install urllib2
