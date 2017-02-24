28b2a575409f5b6887f9f7682e73d70f2f55c035
Sunny <sunny@Sunnys-MacBook-Air.local>
2017-02-24 10:20:46 +0530

    changes in server

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get -y install redis-server
-sudo apt-get install pip
+sudo apt-get install python-pip
 pip install bs4
 pip install redis
 pip install pandas
